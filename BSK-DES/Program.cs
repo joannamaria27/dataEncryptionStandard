3bd9b578f154041212af7786107ab18bf6205f1b
joannamaria27 <joan.mk7@gmail.com>
2020-05-01 21:43:30 +0200

    add function

--- BSK-DES/Program.cs
+++ BSK-DES/Program.cs
@@ -5,84 +5,136 @@ namespace BSK_DES
 {
     class Program
     {
-        //Initial Permutation Table  
+
+
+        public static char[] Permutacja(char[] tablica, int[] tabPer)
+        {
+            char[] tablicaPermutacji = new char[tabPer.Length];
+            int pomocnicza = 0;
+            for (int i = 0; i < tabPer.Length; i++)
+            {
+                pomocnicza = tabPer[i];
+                tablicaPermutacji[i] = tablica[pomocnicza - 1];
+            }
+
+            return tablicaPermutacji;
+        }
+
+        public static char[] Xorowanie(char[] tablicaK, char[] tablicaR)
+        {
+            char[] XOR = new char[48];
+            for (int i = 0; i < 48; i++)
+            {
+                if (tablicaK[i] == tablicaR[i])
+                {
+                    XOR[i] = '0';
+                }
+                else
+                {
+                    XOR[i] = '1';
+                }
+            }
+            return XOR;
+        }
+
+        public static int[] ip = new int[64] { 58,50,42,34,26,18,10,2,
+                                             60,52,44,36,28,20,12,4,
+                                             62,54,46,38,30,22,14,6,
+                                             64,56,48,40,32,24,16,8,
+                                             57,49,41,33,25,17,9,1,
+                                             59,51,43,35,27,19,11,3,
+                                             61,53,45,37,29,21,13,5,
+                                             63,55,47,39,31,23,15,7};
+
+        public static int[] pc = new int[56] { 57,49,41,33,25,17,9,
+                                             1,58,50,42,34,26,18,
+                                             10,2,59,51,43,35,27,
+                                             19,11,3,60,52,44,36,
+                                             63,55,47,39,31,23,15,
+                                             7,62,54,46,38,30,22,
+                                             14,6,61,53,45,37,29,
+                                             21,13,5,28,20,12,4};
+
+        public static int[] przesuniecie6 = new int[16] { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
+
+        public static int[] pc2 = new int[48] { 14,  17, 11, 24,  1,  5,
+                                               3,  28, 15,  6, 21, 10,
+                                               23, 19, 12,  4, 26,  8,
+                                               16,  7, 27, 20, 13,  2,
+                                               41, 52, 31, 37, 47, 55,
+                                               30, 40, 51, 45, 33, 48,
+                                               44, 49, 39, 56, 34, 53,
+                                               46, 42, 50, 36, 29, 32};
+
+
+        public static int[] E = new int[48] { 32,  1,  2,  3,  4,  5,
+                                             4,  5,  6,  7,  8,  9,
+                                             8,  9, 10, 11, 12, 13,
+                                            12, 13, 14, 15, 16, 17,
+                                            16, 17, 18, 19, 20, 21,
+                                            20, 21, 22, 23, 24, 25,
+                                            24, 25, 26, 27, 28, 29,
+                                            28, 29, 30, 31, 32,  1};
+
+
+        public static int[,] S1 = new int[4, 16] { { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7 },
+                                                   { 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8 },
+                                                   { 4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0 },
+                                                   { 15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 } };
+
+
+        public static int[,] S2 = new int[4, 16] { { 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10 },
+                                                   { 3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5 },
+                                                   { 0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15 },
+                                                   { 13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 } };
+
+        public static int[,] S3 = new int[4, 16] { { 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8 },
+                                                   { 13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1 },
+                                                   { 13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7 },
+                                                   { 1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 } };
+
+        public static int[,] S4 = new int[4, 16] { { 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15 },
+                                                   { 13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9 },
+                                                   { 10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4 },
+                                                   { 3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 } };
+
+        public static int[,] S5 = new int[4, 16] { { 2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9 },
+                                                   { 14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6 },
+                                                   { 4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14 },
+                                                   { 11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 } };
+
+        public static int[,] S6 = new int[4, 16] { { 12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11 },
+                                                   { 10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8 },
+                                                   { 9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6 },
+                                                   { 4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 } };
+
+        public static int[,] S7 = new int[4, 16] { { 4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1 },
+                                                   { 13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6 },
+                                                   { 1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2 },
+                                                   { 6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 } };
+
+        public static int[,] S8 = new int[4, 16] { { 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7 },
+                                                   { 1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2 },
+                                                   { 7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8 },
+                                                   { 2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 } };
+
+
+        public static int[] P = new int[32] { 16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25 };
+
+        public static char[] Dzielenie(char[] tablica, int rozmiar, int kolejne)
+        {
+            char[] blok = new char[rozmiar];
+            int l = kolejne;
+            for (int i = 0; i < rozmiar; i++)
+            {
+                blok[i] = tablica[l]; l++;
+            }
+            return blok;
+        }
 
         static void Main(string[] args)
         {
-            int[] ip = new int[] { 58,50,42,34,26,18,10,2,
-                                        60,52,44,36,28,20,12,4,
-                                        62,54,46,38,30,22,14,6,
-                                        64,56,48,40,32,24,16,8,
-                                        57,49,41,33,25,17,9,1,
-                                        59,51,43,35,27,19,11,3,
-                                        61,53,45,37,29,21,13,5,
-                                        63,55,47,39,31,23,15,7 };
-            int[] pc = new int[] { 57, 49, 41, 33, 25, 17,  9,
-                1, 58, 50, 42, 34, 26, 18,
-                10,  2, 59, 51, 43, 35, 27,
-                19, 11,  3, 60, 52, 44, 36,
-                63, 55, 47, 39, 31, 23, 15,
-                7, 62, 54, 46, 38, 30, 22,
-                14,  6, 61, 53, 45, 37, 29,
-                21, 13,  5, 28, 20,12,  4 };
-            int[] przesuniecie6 = new int[] { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1, };
 
-
-            int[] pc2 = new int[] { 14, 17, 11, 24, 1, 5, 3,
-                28, 15, 6, 21, 10, 23,
-                19, 12, 4, 26, 8, 16,
-                7, 27, 20, 13, 2, 41,
-                52, 31, 37, 47, 55, 30,
-                40, 51, 45, 33, 48, 44,
-                49, 39, 56, 34, 53, 46,
-                42, 50, 36, 29, 32 };
-
-
-            int[] E = new int[] { 32, 1, 2, 3, 4, 5,
-                4, 5, 6, 7, 8, 9,
-                8, 9, 10, 11, 12, 13,
-                12, 13, 14, 15, 16, 17,
-                16, 17, 18, 19, 20, 21,
-                20, 21, 22, 23, 24, 25,
-                24, 25, 26, 27, 28, 29,
-                28, 29, 30, 31, 32, 1 };
-
-
-            int[,] S1 = new int[4, 16] { {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7 },
-           {     0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8 },
-           {     4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0 },
-           {     15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 } };
-            int[,] S2 = new int[4, 16] { { 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10 },
-               { 3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5 },
-               {0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15 },
-               { 13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 } };
-            int[,] S3 = new int[4, 16] { { 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8 },
-                { 13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1 },
-                { 13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7 },
-                { 1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 } };
-            int[,] S4 = new int[4, 16] { { 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15 },
-                { 13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9 },
-                { 10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4 },
-                { 3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 } };
-            int[,] S5 = new int[4, 16] { { 2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9 },
-                { 14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6 },
-                { 4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14 },
-                { 11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 } };
-            int[,] S6 = new int[4, 16] { { 12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11 },
-                { 10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8 },
-                { 9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6 },
-                { 4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 } };
-            int[,] S7 = new int[4, 16] { { 4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1 },
-                { 13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6 },
-                { 1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2 },
-                { 6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 } };
-            int[,] S8 = new int[4, 16] { { 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7 },
-                { 1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2 },
-                { 7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8 },
-               { 2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11} };
-
-
-            int[] P = new int[32] { 16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25 };
 
             Console.WriteLine("DES!");
 
@@ -101,68 +153,33 @@ namespace BSK_DES
                     Console.WriteLine("Wprowadz 64 bitowy klucz binarniy: ");
                     string klucz = Console.ReadLine();
                     ////////////////////////podzielenie na bloki 64 bitowe - narazie tylko jeden blok wprowadzany jest
-                    ////////////////////////permutacja wstepna
+
+
                     char[] tablicaPoczatkowa = new char[64];
                     char[] tablicaPoczatkowaKlucz = new char[64];
-                    char[] tablicaPermutacji = new char[64];
-                    char[] tablicaPermutacjiKlucza = new char[64];
+
                     int m = 0;
                     for (int i = 0; i < 64; i++)
                     {
-
                         tablicaPoczatkowa[i] = tekstJawny[m];
                         tablicaPoczatkowaKlucz[i] = tekstJawny[m];
                         m++;
-
-                    }
-                    /////////////////////////tablica permutacji - działa
-                    int pomocnicza;
-
-
-                    for (int i = 0; i < 64; i++)
-                    {
-                        pomocnicza = ip[i];
-                        tablicaPermutacji[i] = tablicaPoczatkowa[pomocnicza - 1];
                     }
 
-                    ///////////////////////////////dzielenie na dwa 32 bloki
-                    char[] blokR = new char[32];
-                    char[] blokL = new char[32];
-                    for (int i = 0; i < 32; i++)
-                    {
-                        blokL[i] = tablicaPermutacji[i];
-                    }
-                    int l = 32;
-                    for (int i = 0; i < 32; i++)
-                    {
-                        blokR[i] = tablicaPermutacji[l]; l++;
-                    }
-                    /////////////////////////klucz - permutacja wstępna
+                    char[] tablicaPermutacji = Permutacja(tablicaPoczatkowa, ip); //2.
+                    char[] blokR = Dzielenie(tablicaPermutacji, 32, 0); //3.
+                    char[] blokL = Dzielenie(tablicaPermutacji, 32, 32); //3.
 
-                    for (int i = 0; i < 56; i++)
-                    {
-                        pomocnicza = pc[i];
-                        tablicaPermutacjiKlucza[i] = tablicaPoczatkowaKlucz[pomocnicza - 1];
-                    }
+                    //klucz 4 -7 
+                    char[] tablicaPermutacjiKlucza = Permutacja(tablicaPoczatkowaKlucz, pc); //4.
+                    char[] kluczC = Dzielenie(tablicaPermutacji, 28, 0); //5.
+                    char[] kluczD = Dzielenie(tablicaPermutacji, 28, 28); //5.
 
-                    ////////////dzielenie na dwie czesci
-                    char[] kluczC = new char[28];
-                    char[] kluczD = new char[28];
-                    for (int i = 0; i < 28; i++)
-                    {
-                        kluczC[i] = tablicaPermutacjiKlucza[i];
-                    }
-                    l = 28;
-                    for (int i = 0; i < 28; i++)
-                    {
-                        kluczD[i] = tablicaPermutacjiKlucza[l]; l++;
-                    }
                     //6.
                     List<char> kluczC6 = new List<char>(kluczC);
                     List<char> kluczD6 = new List<char>(kluczD);
                     char[,] kluczeCpo6 = new char[16, 28];
                     char[,] kluczeDpo6 = new char[16, 28];
-
                     for (int i = 0; i < 16; i++)
                     {
                         for (int j = 0; j < 28; j++)
@@ -178,7 +195,6 @@ namespace BSK_DES
 
                     //7.
                     char[,] K = new char[16, 56];
-                    // List<char> ListaK = new List<char>(K);
                     for (int i = 0; i < 16; i++)
                     {
                         for (int j = 0; j < 28; j++)
@@ -191,8 +207,9 @@ namespace BSK_DES
                             K[i, j] = kluczeDpo6[i, k];
 
                         }
-
                     }
+
+                    int pomocnicza = 0;
                     char[,] tablicaPermutacjiKlucza2 = new char[16, 48];
                     for (int i = 0; i < 16; i++)
                     {
@@ -203,32 +220,10 @@ namespace BSK_DES
                         }
                     }
 
-                    ///8.
-                    char[] blokR8 = new char[48];
-                    for (int j = 0; j < 32; j++)
-                    {
-                        pomocnicza = E[j];
-                        blokR8[j] = blokR[pomocnicza - 1];
-                    }
 
-                    //9. xor k0 i r0
 
-                    char[] Xorowanie(char[] tablicaK, char[] tablicaR)
-                    {
-                        char[] XOR = new char[48];
-                        for (int i = 0; i < 48; i++)
-                        {
-                            if (tablicaK[i] == tablicaR[i])
-                            {
-                                XOR[i] = '0';
-                            }
-                            else
-                            {
-                                XOR[i] = '1';
-                            }
-                        }
-                        return XOR;
-                    }
+                    char[] blokR8 = Permutacja(blokR, E); //8.
+                    //9. xor 
                     char[] tablicaK = new char[48];
                     for (int i = 0; i < 48; i++)
                     {
@@ -300,10 +295,10 @@ namespace BSK_DES
                     char[] ciagR = new char[32];
 
 
-                    int j = 0;
+                    int r = 0;
                     foreach (char i in S6bitowe)
                     {
-                        ciagR[j] = i; j++;
+                        ciagR[r++] = i;
                     }
 
                     //14.
@@ -314,6 +309,7 @@ namespace BSK_DES
                         RPermutacja[i] = ciagR[pomocnicza - 1];
                     }
 
+                    //15.
 
 
 
